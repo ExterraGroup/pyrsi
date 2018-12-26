64a080eaee9c3c873e696e9ac321490f5b67197e
Ventorvar <ventorvar@gmail.com>
2018-12-27 00:17:55 +0100

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ExterraGroup/pyrsi',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=True,
 )
